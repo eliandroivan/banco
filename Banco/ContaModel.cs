5c2728ec2e62d164e48f3f4bf66a47b8f921e38f
eliandroivan <eliandroivan@gmail.com>
2018-11-03 17:29:05 -0300

    inicio da primeira classe

---
+++ Banco/ContaModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class ContaModel
+    {
+        public int numero;
+        public string titular;
+        public double saldo = 020;
+
+        public bool Sacar(double valor)
+        {
+            if ((this.saldo >= valor) && (valor > 0))
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                this.saldo += valor;
+            }
+            
+        }        public void Transferir(double valor, ContaModel contaDestino)
+        {
+            if (this.Sacar(valor))
+            {
+                contaDestino.Depositar(valor);
+            }
+        }
+    }
+}
---
+++ Banco/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ContaModel c = new ContaModel();
+            c.numero = 1;
+            c.Depositar(100.0);            MessageBox.Show(c.saldo.ToString());
+            //	inicializa	os	atributos
+            //	Se	a	conta	tiver	saldo	suficiente,	deuCerto	conterá	o	valor	true
+            //	senão,	ela	conterá	false
+            //bool deuCerto = c.Sacar(100.0);
+            //if (deuCerto)
+            //{
+            //    MessageBox.Show("Saque realizado com sucesso");
+            //    MessageBox.Show(c.saldo.ToString());
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Saldo Insuficiente");
+            //}
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ContaModel contaVictor = new ContaModel();
+            contaVictor.titular = "victor";
+            contaVictor.numero = 1;
+            contaVictor.saldo = 100.0;
+            MessageBox.Show(contaVictor.titular);
+        }
+    }
+}
